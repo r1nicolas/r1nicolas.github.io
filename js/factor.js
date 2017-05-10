77395b434e5af59cbb586ad59d91f1a90fcd62de
r1nicolas <romain.nicolas@hotmail.fr>
2017-05-10 07:04:32 +0200

    Update factor.js

--- js/factor.js
+++ js/factor.js
@@ -1,4 +1,35 @@
+function Factor(number) {
+	if (number == 0)
+		this.factor = null;
+	else if (number == 1)
+		this.factor = array();
+	else {
+		this.factor = array();
+		if (number < 0) {
+			this.factor[] = -1;
+			number = -number;
+		}
+		var i = 2;
+		while (i * i <= number) {
+			while (number % i == 0) {
+				this.factor[] = i;
+				number /= i;
+			}
+			i++;
+		}
+	}
+}
+
+/*factor.prototype.toString = function() {
+	if (this.factor == null)
+		return "0";
+	if ()
+}*/
+
 function factorize() {
 	var n = document.getElementById('number').value;
-	document.getElementById('display').innerHTML = n;
+	var text = n + ": ";
+	var factor = new Factor(n);
+	console.log(factor);
+	document.getElementById('display').innerHTML = text;
 }
