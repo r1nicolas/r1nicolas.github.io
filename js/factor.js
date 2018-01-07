04bfa3f38530bb131902d78412ea8be81c785c3c
r1nicolas <romain.nicolas@hotmail.fr>
2018-01-07 05:04:12 +0100

    Update factor.js

--- js/factor.js
+++ js/factor.js
@@ -109,11 +109,12 @@ Factor.prototype.getButton = function() {
 	var ret = "";
 	i = 0
 	if (this.neg) {
-		ret += '<span class="square"><p class="close"><i class="fa fa-times point" aria-hidden="true" onclick="remove('+ i + ', ' + this.factor[i] + ')"></i></p>';
-		ret += '<p class="center"><i class="fa fa-arrow-left point" aria-hidden="true" onclick="moveLeft('+ i + ')"></i> ' + this.factor[i] + ' <i class="fa fa-arrow-right point" aria-hidden="true" onclick="moveRight('+ i + ')"></i></p>';
+		ret += '<span class="square"><p class="close"><i class="fa fa-times point" aria-hidden="true" onclick="div(-1)"></i></p>';
+		ret += '<p class="center">-1</p>';
 		ret += '<p><i class="fa fa-pencil-square-o point" aria-hidden="true"></i></p></span> ';
+		i++;
 	}
-	for(;i < this.factor.length + (this.neg ? 1 : 0);i++) {
+	for(;i < this.factor.length;i++) {
 		ret += '<span class="square"><p class="close"><i class="fa fa-times point" aria-hidden="true" onclick="remove('+ i + ', ' + this.factor[i] + ')"></i></p>';
 		ret += '<p class="center"><i class="fa fa-arrow-left point" aria-hidden="true" onclick="moveLeft('+ i + ')"></i> ' + this.factor[i] + ' <i class="fa fa-arrow-right point" aria-hidden="true" onclick="moveRight('+ i + ')"></i></p>';
 		ret += '<p><i class="fa fa-pencil-square-o point" aria-hidden="true"></i></p></span> ';
