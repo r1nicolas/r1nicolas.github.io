5cbd3e99d09b3cd49cd23277c0d9c687051d48d1
r1nicolas <romain.nicolas@hotmail.fr>
2022-02-23 23:17:27 +0100

    Update vortex.js

--- js/vortex.js
+++ js/vortex.js
@@ -1,4 +1,6 @@
 var vortex = document.getElementById('vortex');
+vortex.width = 600;
+vortex.height = 600;
 var ctx = vortex.getContext('2d');
 ctx.beginPath();
 ctx.arc(275, 275, 250, 0, Math.PI * 2, true);  // Cercle extérieur
