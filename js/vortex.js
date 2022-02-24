674c288cf29c864bfde296bc42ecfb4767b4ac4d
r1nicolas <romain.nicolas@hotmail.fr>
2022-02-24 09:51:21 +0100

    Update vortex.js

--- js/vortex.js
+++ js/vortex.js
@@ -14,13 +14,14 @@ function drawVortex() {
 	if (modulo < 2 || multiplier < 0)
 		return;
 	point[0] = {x:275, y:25, d:0};
-	for(var i = 1;i < modulo;i++) {
-		point[i] = {d:(i * multiplier) % modulo, x:275 + 250 * Math.sin(i * 2 * Math.PI / modulo), y:275 - 250 * Math.cos(i * 2 * Math.PI / modulo)};
-	}
 	ctx.clearRect(0, 0, 600, 600);
 	ctx.beginPath();
 	ctx.arc(275, 275, 250, 0, Math.PI * 2, true);  // Cercle extérieur
 	ctx.stroke();
+	ctx.strokeText(0, 275, 15);
+	for(var i = 1;i < modulo;i++) {
+		point[i] = {d:(i * multiplier) % modulo, x:275 + 250 * Math.sin(i * 2 * Math.PI / modulo), y:275 - 250 * Math.cos(i * 2 * Math.PI / modulo)};
+	}
 	for(var i = 1;i < modulo;i++) {
 		ctx.beginPath();
 		ctx.moveTo(point[i].x, point[i].y);
