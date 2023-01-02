ef06144661afc63edfecec9b90178ac5a57d81ff
r1nicolas <romain.nicolas@hotmail.fr>
2023-01-03 00:00:19 +0100

    Update factor.js

--- js/factor.js
+++ js/factor.js
@@ -1,7 +1,3 @@
-/*
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- */
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
